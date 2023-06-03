01e61dcdbf02d7ef909499b7a9e36bc1a45a7e41
vastidev <Vastiane1@gmail.com>
2023-06-03 16:01:29 -0300

    init commit

---
+++ src/main/java/dev/wakandaacademy/crc_odonto/CrcOdontoApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.crc_odonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrcOdontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrcOdontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/crc_odonto/CrcOdontoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.crc_odonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrcOdontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
